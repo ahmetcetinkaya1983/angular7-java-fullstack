aef715a20608136c5ff77b02a95cd27a61f0c0a7
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-04-13 15:34:53 +0300

    error handligng back-end and front-end

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -9,11 +10,39 @@ import { ActivatedRoute } from '@angular/router';
 export class WelcomeComponent implements OnInit {
 
   name='';
+  message = 'Some welcome message';
+  welcomeMessageFromService : string;
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,
+              private service:WelcomeDataService) { }
 
   ngOnInit() {
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMessage(){
+    //console.log("get welcome messsage");
+    console.log(this.service.executeHelloWorldBeanService());
+
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+
+
+  }
+
+  handleSuccessfulResponse(response){
+    this.welcomeMessageFromService = response.message;
+    console.log(response);
+    console.log(response.message);
+  }
+
+  handleErrorResponse(error) {
+    console.log(error);
+    console.log(error.error);
+    console.log(error.error.message);
+    this.welcomeMessageFromService = error.error.message;
   }
 
 }
